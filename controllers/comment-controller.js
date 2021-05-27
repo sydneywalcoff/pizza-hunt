b8f81e74b267aaffea279e5859d9e82131a64c90
Sydney Walcoff <sydney.walcoff@gmail.com>
2021-05-27 11:03:10 -0700

    added removeReply controller and updated routes

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -22,6 +22,23 @@ const commentController = {
             .catch(err => res.json(err));
     },
 
+    // add reply
+    addReply({ params, body }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+        .then(dbPizzaData => {
+            if(!dbPizzaData) {
+                res.status(404).json({ message: 'No pizza found with this id!' });
+                return;
+            }
+            res.json(dbPizzaData);
+        })
+        .catch(err => res.json(err));
+    },
+
     // remove comment
     removeComment({ params }, res) {
         Comment.findOneAndDelete({ _id: params.commentId })
